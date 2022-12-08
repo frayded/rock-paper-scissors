61a8a209c208d24311b777f0720fd4a2d95ed061
frayded <frayded99@gmail.com>
2022-12-08 15:12:58 +0700

    finalizing, adding scores and results, some fixes w/ one round code

--- index.js
+++ index.js
@@ -46,31 +46,33 @@ console.log("Game over, you won " + playerScore + " rounds, lose " + computerSco
 // The following is the code for one round rock paper scissors game as instructed, I put the working code in the game function for less redundancy
 /*
 let playerSelection = prompt("Shall we play a game of rock, paper, scissors? Enter your choice!").toLowerCase();
-console.log(playerSelection);
+        console.log("You choose " + playerSelection);
 
-function getComputerChoice() {
-    const gameChoices = ["rock", "paper", "scissors"];
-    let computerChoice = Math.floor(Math.random() * gameChoices.length);
 
-    return gameChoices[computerChoice];
-}
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
+        function getComputerChoice() {
+            const gameChoices = ["rock", "paper", "scissors"];
+            let computerChoice = Math.floor(Math.random() * gameChoices.length);
+        
+            return gameChoices[computerChoice];
+        }
+        let computerSelection = getComputerChoice();
+        console.log("Computer chooses " + computerSelection);
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
-        computerScore++
-        return "Uh oh! You lose this round!"
-    } else if (playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "rock" && computerSelection == "scissors") {
-        playerScore++
-        return "Yeay! You win this round!"
-    } else if (playerSelection == "rock" && computerSelection == "rock" || playerSelection == "paper" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "scissors") {
-        tieScore++
-        return "This round is a tie!"
-    } else {
-        return "Please input the proper choice."
-    }
-}
 
-console.log(playRound(playerSelection, computerSelection));
+        function playRound(playerSelection, computerSelection) {
+            if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
+                computerScore++
+                return "Uh oh! You lose this round!"
+            } else if (playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper" || playerSelection == "rock" && computerSelection == "scissors") {
+                playerScore++
+                return "Yeay! You win this round!"
+            } else if (playerSelection == "rock" && computerSelection == "rock" || playerSelection == "paper" && computerSelection == "paper" || playerSelection == "scissors" && computerSelection == "scissors") {
+                tieScore++
+                return "This round is a tie!"
+            } else {
+                failInput++
+                return "Please input the proper choice."
+            }
+        }
+        console.log(playRound(playerSelection, computerSelection));
 */
