5a9ad5f9bee70320033f0abbb4bc70dacd345f98
frayded <frayded99@gmail.com>
2022-12-08 16:09:33 +0700

    formatting

--- index.js
+++ index.js
@@ -5,65 +5,56 @@ let tieScore = 0;
 let failInput = 0;
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt(
-      "Shall we play a game of rock, paper, scissors? Enter your choice!"
-    ).toLowerCase();
-    console.log("You choose " + playerSelection);
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt(
+            "Shall we play a game of rock, paper, scissors? Enter your choice!"
+        ).toLowerCase();
+        console.log("You choose " + playerSelection);
 
-    function getComputerChoice() {
-      const gameChoices = ["rock", "paper", "scissors"];
-      let computerChoice = Math.floor(Math.random() * gameChoices.length);
+        function getComputerChoice() {
+            const gameChoices = ["rock", "paper", "scissors"];
+            let computerChoice = Math.floor(Math.random() * gameChoices.length);
 
-      return gameChoices[computerChoice];
+            return gameChoices[computerChoice];
+        }
+        let computerSelection = getComputerChoice();
+        console.log("Computer chooses " + computerSelection);
+
+        function playRound(playerSelection, computerSelection) {
+            if (
+                (playerSelection == "rock" && computerSelection == "paper") ||
+                (playerSelection == "paper" && computerSelection == "scissors") ||
+                (playerSelection == "scissors" && computerSelection == "rock")
+            ) {
+                computerScore++;
+                return "Uh oh! You lose this round!";
+            } else if (
+                (playerSelection == "paper" && computerSelection == "rock") ||
+                (playerSelection == "scissors" && computerSelection == "paper") ||
+                (playerSelection == "rock" && computerSelection == "scissors")
+            ) {
+                playerScore++;
+                return "Yeay! You win this round!";
+            } else if (
+                (playerSelection == "rock" && computerSelection == "rock") ||
+                (playerSelection == "paper" && computerSelection == "paper") ||
+                (playerSelection == "scissors" && computerSelection == "scissors")
+            ) {
+                tieScore++;
+                return "This round is a tie!";
+            } else {
+                failInput++;
+                return "Please input the proper choice.";
+            }
+        }
+        console.log(playRound(playerSelection, computerSelection));
     }
-    let computerSelection = getComputerChoice();
-    console.log("Computer chooses " + computerSelection);
-
-    function playRound(playerSelection, computerSelection) {
-      if (
-        (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "rock")
-      ) {
-        computerScore++;
-        return "Uh oh! You lose this round!";
-      } else if (
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "rock" && computerSelection == "scissors")
-      ) {
-        playerScore++;
-        return "Yeay! You win this round!";
-      } else if (
-        (playerSelection == "rock" && computerSelection == "rock") ||
-        (playerSelection == "paper" && computerSelection == "paper") ||
-        (playerSelection == "scissors" && computerSelection == "scissors")
-      ) {
-        tieScore++;
-        return "This round is a tie!";
-      } else {
-        failInput++;
-        return "Please input the proper choice.";
-      }
-    }
-    console.log(playRound(playerSelection, computerSelection));
-  }
 }
 
 console.log(game());
 
 console.log(
-  "Game over, you won " +
-    playerScore +
-    " rounds, lose " +
-    computerScore +
-    " rounds, and tied for " +
-    tieScore +
-    " rounds!\n" +
-    "Also, made " +
-    failInput +
-    " typos, just so you know."
+    "Game over, you won " + playerScore + " rounds, lose " + computerScore + " rounds, and tied for " + tieScore + " rounds!\n" + "Also, made " + failInput + " typos, just so you know."
 );
 
 // The following is the code for one round rock paper scissors game as instructed, I put the working code in the game function for less redundancy
